0ca14d299c5c8414084e4dcee9d5b59474a61591
Mauricio Graciano <mau.graci@gmail.com>
2022-05-02 12:41:10 -0500

    Add fontawesome icons

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import { createApp } from 'vue';
+import {
+  faBriefcase,
+  faIcons,
+  faMusic,
+} from '@fortawesome/free-solid-svg-icons';
+import { dom, library } from '@fortawesome/fontawesome-svg-core';
+
 import App from './App.vue';
 
 import 'normalize.css';
+
 import './assets/base.scss';
 
+library.add(faBriefcase, faIcons, faMusic);
+
 createApp(App).mount('#app');
+
+dom.watch();
